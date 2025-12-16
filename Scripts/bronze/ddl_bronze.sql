c44977a25871393ffe1d96d07357fb1ea0f6e53c
Avishka Edirisuriya <129731084+AvishkaEdirisuriya@users.noreply.github.com>
2025-12-16 14:14:04 +0530

    Update ddl_bronze.sql

--- Scripts/bronze/ddl_bronze.sql
+++ Scripts/bronze/ddl_bronze.sql
@@ -7,7 +7,7 @@ Puporse of the Script:
 	Optional commented lines allow you to check whether a table already exists 
 	and drop it before recreating it if necessary.
 */
-  
+
 /*
 -- Check weather if the table exists or not, if exists drop it and create a new one --
 
@@ -15,6 +15,8 @@ IF OBJECT_id ('bronze.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info
 */
 
+IF OBJECT_id ('bronze.crm_cust_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info
 CREATE TABLE bronze.crm_cust_info(
 	cst_id				INT,
 	cst_keY				NVARCHAR(50),
@@ -25,6 +27,8 @@ CREATE TABLE bronze.crm_cust_info(
 	cst_create_date		DATE
 );
 
+IF OBJECT_id ('bronze.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info
 CREATE TABLE bronze.crm_prd_info (
 	prd_id			INT,
 	prd_keY			NVARCHAR(50),
@@ -35,6 +39,8 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_end_dt		DATETIME
 );
 
+IF OBJECT_id ('bronze.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_keY		NVARCHAR(50),
@@ -47,17 +53,23 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_price		INT
 );
 
+IF OBJECT_id ('bronze.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12
 CREATE TABLE bronze.erp_cust_az12 (
 	cid		NVARCHAR(50),
 	bdate	DATE,
 	gen		NVARCHAR(50)
 );
 
+IF OBJECT_id ('bronze.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101
 CREATE TABLE bronze.erp_loc_a101 (
 	cid		NVARCHAR(50),
 	cntry	NVARCHAR(50)
 );
 
+IF OBJECT_id ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 	id			NVARCHAR(50),
 	cat			NVARCHAR(50),
